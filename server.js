e16ebd33721139c51589af142ca1ce8ff45357fe
Jonathan Navarrete <jonatns@gmail.com>
2018-06-21 23:28:04 -0700

    Fixed some stuff

--- server.js
+++ server.js
@@ -10,13 +10,9 @@ const upload = multer();
 app.use(cors());
 
 const Ftp = new jsftp({
-  host: process.env.FTP_URL,
+  host: process.env.FTP_HOST,
   user: process.env.USER_NAME,
   pass: process.env.PASSWORD
-});
-
-Ftp.list("./wwwroot/UploadFolder", (err, res) => {
-  console.log(res);
 });
 
 const generateFileName = () => {
@@ -30,12 +26,16 @@ const generateFileName = () => {
   );
 };
 
+app.get("*", (req, res) => {
+  res.send("FTP File Uploader");
+});
+
 app.post("/upload", upload.single("image"), (req, res) => {
   const { buffer, mimetype } = req.file;
   const fileName = generateFileName();
   const ext = mimetype.split("/")[1];
 
-  Ftp.put(buffer, `./wwwroot/UploadFolder/${fileName}.${ext}`, err => {
+  Ftp.put(buffer, `${process.env.FTP_UPLOAD_PATH}${fileName}.${ext}`, err => {
     if (!err) {
       res.status(200).send("Image uploaded successfully!");
     } else {
